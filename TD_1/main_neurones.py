9e7e523b547519efde199b511c39fd5c54226746
RNiermarechal <robin.niermarechal@ecl19.ec-lyon.fr>
2021-11-04 23:09:01 +0100

    Avancement neurones

--- TD_1/main_neurones.py
+++ TD_1/main_neurones.py
@@ -3,9 +3,9 @@ import skimage
 from main_kppv import unpickle,lecture_cifar
 np.random.seed(1) # pour que l'exécution soit déterministe
 
-path='D:/Robin Niermaréchal/Documents/ECL/3A/S9/MOD/IA/TD_1/cifar-10-batches-py/data_batch_1'
+# path='D:/Robin Niermaréchal/Documents/ECL/3A/S9/MOD/IA/TD_1/cifar-10-batches-py/data_batch_1'
 
-X,Y=lecture_cifar(path)
+# X,Y=lecture_cifar(path)
 
 ##########################
 # Génération des données #
@@ -26,9 +26,10 @@ Y = np.random.random((N, D_out))
 W1 = 2 * np.random.random((D_in, D_h)) - 1
 b1 = np.zeros((1,D_h))
 W2 = 2 * np.random.random((D_h, D_out)) - 1
-
 b2 = np.zeros((1,D_out))
 
+
+# def forward(X,W1,b1,W2,b2,Y):
 ####################################################
 # Passe avant : calcul de la sortie prédite Y_pred #
 ####################################################
@@ -44,3 +45,30 @@ Y_pred = O2 # Les valeurs prédites sont les sorties de la couche de sortie
 ########################################################
 loss = np.square(Y_pred - Y).sum() / 2
 print(loss)
+    # return Y_pred,loss
+
+
+## Back propagation
+lr=1e-4 # learning rate
+n_iter= 2000 # iterations pour apprentissage du modele
+
+def back_propagate(X,Y,O1,O2,W1,W2,B1,B2,lr):
+    dL_dI2=(O2-Y)*((1-O2)*O2)
+    dL_dw2=np.dot(np.transpose(O1),dL_dI2)
+    dO1_dw1=np.dot(np.diagflat((1-O1)*O1),np.kron(np.eye(O1.shape[-1]),X)) #300x20
+    dI2_dw1=np.dot(np.kron(np.transpose(W2),np.ones(X.shape[0])),dO1_dw1)
+    # dI2_dw1=np.dot(np.transpose(W2),dO1_dw1)
+    dL_dw1=np.dot(dL_dI2,dI2_dw1)
+
+    dL_db2=dL_dI2
+
+    # Update des parametres du modele
+    w1=W1-dL_dw1*lr# pour w1
+    # pour b1
+    w2=W2-dL_dw2*lr# pour w2
+    # pour b2
+    b2=B2-dL_db2*lr
+
+    return w2,b2
+
+back_propagate(X, Y, O1, O2, W1, W2, b1, b2, lr)
